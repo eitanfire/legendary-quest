645242aa1400f033142ff45f3090e98475d9841d
Eitan Fire <eitanfire@gmail.com>
2023-04-22 18:00:03 -0600

    update COURSES information

--- src/app/shared/COURSES.js
+++ src/app/shared/COURSES.js
@@ -34,7 +34,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "The study of the fundamental nature of knowledge, reality, and existence.",
+      "[World History] The study of the fundamental nature of knowledge, reality, and existence.",
   },
   {
     id: 1,
@@ -49,7 +49,7 @@ export const COURSES = [
     semester: 2,
     featured: true,
     description:
-      "This class will focus on the first and second world wars. We will start by examining the roots of the wars. We will learn about this topic in many different ways including through reading, writing, discussion, film, and role-playing. This is an opportunity to increase your understanding of the World Wars whether you bring just a little knowledge or whether you consider yourself already highly informed on this topic. We will try to meet outside when the weather permits.",
+      "[World History][US History Credit] Focusing on the causes, effects, and fascinating details of WWI and WWII. This class will focus on the first and second world wars. We will start by examining the roots of the wars. We will learn about this topic in many different ways including through reading, writing, discussion, film, and role-playing.",
   },
   {
     id: 2,
@@ -64,7 +64,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "Economics studies the choices that we make. What do we do in a world with limited resources and unlimited wants? This class will begin by focusing with a micro-focus on personal finance examining such topics as savings and taxes. Students will identify what kind of careers they might be well-suited for and feel passionate about. They will have the opportunity to identify their passion and channel it into a dream job. The course will continue by studying macroeconomics as we learn about globalization, trade, and development.",
+      "[Elective] Economics studies the choices that we make. What do we do in a world with limited resources and unlimited wants? This class will begin by focusing with a micro-focus on personal finance examining such topics as savings and taxes. Students will identify what kind of careers they might be well-suited for and feel passionate about. They will have the opportunity to identify their passion and channel it into a dream job. The course will continue by studying macroeconomics as we learn about globalization, trade, and development.",
   },
   {
     id: 3,
@@ -79,7 +79,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "This class will focus on the history of policing in the United States of America. The modalities for learning about this topic include reading, writing, discussion, film, and simulations. We will aim to ground our learning beyond the classroom through a field trip to the Justice Center as well as hearing from a public defender among other learning activities. We will aim to meet outside when the weather permits.",
+      "[US History] This class will focus on the history of policing in the United States of America. The modalities for learning about this topic include reading, writing, discussion, film, and simulations. We will aim to ground our learning beyond the classroom through a field trip to the Justice Center as well as hearing from a public defender among other learning activities. We will aim to meet outside when the weather permits.",
     freeContentDescription: "Speak up. Teach about what matters.",
     free: true,
   },
@@ -96,7 +96,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "We will learn to unpack and understand film the same way a literature class studies text or an art class examines art. Students will become proficient in their understanding of film techniques. Learners in this class will have an opportunity to do their own research on film directors. Students will express their own original ideas through the process of drafting storyboards as well as screenplays to make their own stop motion animations as well as live-action movies.",
+      "[Elective] We will learn to unpack and understand film the same way a literature class studies text or an art class examines art. Students will become proficient in their understanding of film techniques. Learners in this class will have an opportunity to do their own research on film directors. Students will express their own original ideas through the process of drafting storyboards as well as screenplays to make their own stop-motion animations as well as live-action movies.",
     free: false,
   },
   {
@@ -112,7 +112,7 @@ export const COURSES = [
     semester: 1,
     featured: false,
     description:
-      "This course is designed to start you on a path toward future studies in web development, design, and data science no matter how little experience or technical knowledge you currently have.By the end you’ll be able to describe the structure and functionality of the world wide web and to create dynamic web pages using a combination of HTML, CSS, and JavaScript.",
+      "[Elective] This course is designed to start you on a path toward future studies in web development, design, and data science no matter how little experience or technical knowledge you currently have.By the end you’ll be able to describe the structure and functionality of the world wide web and to create dynamic web pages using a combination of HTML, CSS, and JavaScript.",
   },
   {
     id: 6,
@@ -127,7 +127,7 @@ export const COURSES = [
     semester: 2,
     featured: true,
     description:
-      "From the Mahabharata to the Odyssey epic poems are mysterious stories with ancient origins that contain a wealth of insight regarding what it means to be human and the journey we all go on in life. This class will examine relevant historical connections to these stories and study these poems themselves through excerpts, reinterpretations through graphic novels, roleplay, and art.",
+      "[World History][Language Arts] From the Mahabharata to the Odyssey epic poems are mysterious stories with ancient origins that contain a wealth of insight regarding what it means to be human and the journey we all go on in life. This class will examine relevant historical connections to these stories and study these poems themselves through excerpts, reinterpretations through graphic novels, roleplay, and art.",
   },
   {
     id: 7,
@@ -142,7 +142,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "Geography is the study of the physical features of the earth and its atmosphere, and of human activity, as it affects and is affected by these, including the distribution of populations and resources, land use, and industries. We will examine this subject through the lens of survival considering how we can deal with our often harsh world. We will participate in the RISE Challenge, a competition to make our community more resilient to natural disasters depending upon students’ interests.",
+      "[Geography] This is a geography class. Geography is the study of the physical features of the earth and its atmosphere, and of human activity, as it affects and is affected by these, including the distribution of populations and resources, land use, and industries. We will examine this subject through the lens of survival considering how we can deal with our often harsh world. We will participate in the RISE Challenge, a competition to make our community more resilient to natural disasters depending upon students’ interests.",
   },
   {
     id: 8,
@@ -157,7 +157,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "Pirates continue to excite and fascinate us. What has led to the appearance of these outlaws throughout time? What is the truth behind the myths that we know about pirates? This course explores world history, US history, geography, and physical education through the lens of pirates.",
+      "[World History][US History] Pirates continue to excite and fascinate us. What has led to the appearance of these outlaws throughout time? What is the truth behind the myths that we know about pirates? This course explores world history, US history, geography, and physical education through the lens of pirates.",
   },
   {
     id: 9,
@@ -172,7 +172,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "The history of the United States in the 1800s has been described as “flyover country” by some historians. While often neglected, the events of this time are crucial to understanding some of the most critical issues of our day. Studying this period of time is both recognizably modern and bizarre and alien at times. This class aims to enlighten students about a period of time that precious few understand.",
+      "[US History] The history of the United States in the 1800s has been described as “flyover country” by some historians. While often neglected, the events of this time are crucial to understanding some of the most critical issues of our day. Studying this period of time is both recognizably modern and bizarre and alien at times. This class aims to enlighten students about a period of time that precious few understand.",
   },
   {
     id: 10,
@@ -187,7 +187,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "Students will study the purposes, principles, and practices of the American government as established by the Constitution. Students are expected to understand their rights and responsibilities as citizens and how to exercise these rights and responsibilities in local, state, and national government. Students will learn the structure and processes of the government of the state of Colorado and various local governments.",
+      "[Government] Students will study the purposes, principles, and practices of the American government as established by the Constitution. Students are expected to understand their rights and responsibilities as citizens and how to exercise these rights and responsibilities in local, state, and national government. Students will learn the structure and processes of the government of the state of Colorado and various local governments.",
   },
   {
     id: 11,
@@ -202,7 +202,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "This class aims to reveal the geographic and historical forces that shape the issues of immigration today. The main units of study of this course will focus on issues of immigration in North America ( such as Mexico and the Northern Triangle), Africa, the Middle East (Such as the Israeli and Palestinian conflict and the Syrian conflict), and Asia (Including India and Pakistan.)",
+      "[World History][Geography Credit][Government Credit] This class will explore the role of migration on the local and global scale as well as the increasing role of technology in our lives. This class aims to reveal the geographic and historical forces that shape the issues of immigration today. The main units of study of this course will focus on issues of immigration in North America ( such as Mexico and the Northern Triangle), Africa, the Middle East (Such as the Israeli and Palestinian conflict and the Syrian conflict), and Asia (Including India and Pakistan.)",
   },
   {
     id: 12,
@@ -217,7 +217,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "Never have I witnessed such sincere hospitality and the overwhelming spirit of true brotherhood as is practiced by people of all colours and races here in this ancient Holy Land, the home of Abraham, Muhammad, and all the other prophets of the Holy Scriptures. For the past week, I have been utterly speechless and spellbound by the graciousness I see displayed all around me by people of all colours.~ Malcolm X,The Holy Land... What an experience ~ Justin Timberlake",
+      "[World history credit] This class will focus on the hotly contested region of the Levant and look at how this region has been formed by religious, historical, and geographic forces. Never have I witnessed such sincere hospitality and the overwhelming spirit of true brotherhood as is practiced by people of all colours and races here in this ancient Holy Land, the home of Abraham, Muhammad, and all the other prophets of the Holy Scriptures. For the past week, I have been utterly speechless and spellbound by the graciousness I see displayed all around me by people of all colours.~ Malcolm X,The Holy Land... What an experience ~ Justin Timberlake",
   },
   {
     id: 13,
@@ -232,7 +232,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "This course will explore an array of diseases and how they have altered the course of history. Embedded in this curriculum is the history of science, intellectual history, as well as themes of social justice. ",
+      "[World History]Tracing the impact of infectious diseases on societies throughout time. This course will explore an array of diseases and how they have altered the course of history. Embedded in this curriculum is the history of science, intellectual history, as well as themes of social justice.",
   },
   {
     id: 14,
@@ -247,7 +247,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "This class examines the epic military and ideological clash that occurred between the US and the USSR during the 20th century.",
+      "[World History][US History] This class examines the epic military and ideological clash that occurred between the US and the USSR during the 20th century.",
   },
   {
     id: 15,
@@ -262,7 +262,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "Geography is the study of the physical features of the earth and its atmosphere, and of human activity, as it affects and is affected by these, including the distribution of populations and resources, land use, and industries. We will examine this subject through the lens of survival considering how we can deal with our often harsh world. We will participate in the RISE Challenge, a competition to make our community more resilient to natural disasters depending upon students’ interests.",
+      "[World History] This course will cover the history and cultures of the ancient world. The primary focus will be on Mesopotamia, Egypt, China, India, the Greek civilizations, and the rise and fall of the Roman Empire. The history, geography, cultural identity, and impact on the modern world will be studied in-depth. Students will reinforce map skills, interpret mythology, and strengthen written and verbal communication of historical information.",
   },
   {
     id: 16,
@@ -277,7 +277,7 @@ export const COURSES = [
     semester: 2,
     featured: false,
     description:
-      "This course will develop and improve public speaking and critical thinking skills. Students will prepare and deliver speeches and participate in debates and forums on a variety of  topics.",
+      "[Elective] This course will develop and improve public speaking and critical thinking skills. Students will prepare and deliver speeches and participate in debates and forums on a variety of topics.",
   },
   {
     id: 17,
