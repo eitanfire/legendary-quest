5c099c5d4fb4f0a0ca235d3f5f8c54f881602da8
Eitan Fire <eitanfire@gmail.com>
2023-05-05 21:40:20 -0600

    Credit.js

---
+++ src/features/courses/creditType/Credit.js
@@ -0,0 +1,11 @@
+import { Col } from "reactstrap";
+import 'Credit.css';
+import COURSES from '../../../app/shared/COURSES';
+
+const Credit = ({ credit }) => {
+return (
+    <Col>{credit}</Col>
+)
+}
+
+export default Credit;
