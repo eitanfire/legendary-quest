0d375b998b55784b0f1deeab1f2e280e49ea8f05
Eitan Fire <eitanfire@gmail.com>
2023-06-07 23:01:13 -0600

    Add form validation to UserLoginForm

--- src/features/user/UserLoginForm.js
+++ src/features/user/UserLoginForm.js
@@ -9,8 +9,9 @@ import {
   Label,
   Button,
 } from "reactstrap";
-import { Formik, Field, Form } from "formik";
+import { Formik, Field, Form, ErrorMessage } from "formik";
 import defaultAvatar from "../../app/assets/img/tlLogo.jpeg";
+import { validateUserLoginForm } from "../../utils/validateUserLoginForm";
 
 const UserLoginForm = () => {
   const [loginModalOpen, setLoginModalOpen] = useState(false);
@@ -28,66 +29,71 @@ const UserLoginForm = () => {
   };
 
   return (
-      <>
-        <span className="navbar-text ml-auto">
-          {currentUser ? (
-            <div style={{ width: "4rem", height: "4rem" }}>
-              <img
-                src={currentUser.avatar}
-                alt={"user"}
-                style={{ width: "100%", height: "100%" }}
-              />
-            </div>
-          ) : (
-            <Button
-              outline
-              onClick={() => setLoginModalOpen(true)}
-              style={{ color: "white", border: "1px solid white" }}
-            >
-              <i className="fa fa-sign-in fa-lg" /> Login
-            </Button>
-          )}
-        </span>
-        <Modal isOpen={loginModalOpen}>
-          <ModalHeader toggle={() => setLoginModalOpen(false)}>
-            Login
-          </ModalHeader>
-          <ModalBody>
-            <Formik
-              initialValues={{ username: "", password: "" }}
-              onSubmit={handleLogin}
-            >
-              <Form>
-                <FormGroup>
-                  <Label htmlFor="username">
-                    Username
-                    <Field
-                      id="username"
-                      name="username"
-                      placeholder="Username"
-                      className="form-control"
-                    />
-                  </Label>
-                </FormGroup>
-                <FormGroup>
-                  <Label htmlFor="password">
-                    Password
-                    <Field
-                      id="password"
-                      name="password"
-                      placeholder="Password"
-                      className="form-control"
-                    />
-                  </Label>
-                </FormGroup>
-                <Button type="submit" color="primary">
-                  Login
-                </Button>
-              </Form>
-            </Formik>
-          </ModalBody>
-        </Modal>
-      </>
+    <>
+      <span className="navbar-text ml-auto">
+        {currentUser ? (
+          <div style={{ width: "4rem", height: "4rem" }}>
+            <img
+              src={currentUser.avatar}
+              alt={"user"}
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+        ) : (
+          <Button
+            outline
+            onClick={() => setLoginModalOpen(true)}
+            style={{ color: "white", border: "1px solid white" }}
+          >
+            <i className="fa fa-sign-in fa-lg" /> Login
+          </Button>
+        )}
+      </span>
+      <Modal isOpen={loginModalOpen}>
+        <ModalHeader toggle={() => setLoginModalOpen(false)}>Login</ModalHeader>
+        <ModalBody>
+          <Formik
+            initialValues={{ username: "", password: "" }}
+            onSubmit={handleLogin}
+            validate={validateUserLoginForm}
+          >
+            <Form>
+              <FormGroup>
+                <Label htmlFor="username">
+                  Username
+                  <Field
+                    id="username"
+                    name="username"
+                    placeholder="Username"
+                    className="form-control"
+                  />
+                  <ErrorMessage name="username">
+                    {(msg) => <p className="text-danger">{msg}</p>}
+                  </ErrorMessage>
+                </Label>
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">
+                  Password
+                  <Field
+                    id="password"
+                    name="password"
+                    placeholder="Password"
+                    className="form-control"
+                  />
+                  <ErrorMessage name="password">
+                    {(msg) => <p className="text-danger">{msg}</p>}
+                  </ErrorMessage>
+                </Label>
+              </FormGroup>
+              <Button type="submit" color="primary">
+                Login
+              </Button>
+            </Form>
+          </Formik>
+        </ModalBody>
+      </Modal>
+    </>
   );
 };
 
