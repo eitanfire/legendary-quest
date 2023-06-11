458e0d18b5995e6f3b733168f9aa6fa85b40245f
Eitan Fire <eitanfire@gmail.com>
2023-06-11 11:23:57 -0600

    style LoadMoreCourses

--- src/utils/loadMoreCourses.js
+++ src/utils/loadMoreCourses.js
@@ -1,51 +1,33 @@
-import CoursesList from "../features/courses/CoursesList";
 import React, { useState } from "react";
-import { Button } from "reactstrap";
+import { Button, Col, Row } from "reactstrap";
 import { COURSES } from "../app/shared/COURSES";
 import CourseCard from "../features/courses/CourseCard";
-import { Col, Row } from "reactstrap";
-// import { useSelector } from "react-redux";
-// import { selectAllCourses } from "../features/courses/coursesSlice";
-
 
 const coursePerRow = 4;
- const LoadMoreCourses = ({ courses }) => {
-   const [next, setNext] = useState(coursePerRow);
-   // const courses = useSelector(selectAllCourses);
 
-   const handleMoreCourse = () => {
-     setNext(next + coursePerRow);
-   };
-   return (
-     <>
-       <div className="gap-y-4 flex flex-wrap justify-center">
-         {COURSES?.slice(0, next)?.map((course, id) => {
-           {
-             /* return (
-            <div key={index} className="px-2.5 md:px-0">
-              <img className="cursor-pointer" src={course?.id} />
-            </div>
-          );
-        })} */
-             return (
-               <Row className="ms-auto">
-                 <Col md="5" className="m-4" key={course.id}>
-                   <CourseCard course={course} />
-                 </Col>
-               </Row>
-             );
-           }
-         })}
-         <div>
-           {next < COURSES?.length && (
-             <Button className="mt-4" onClick={handleMoreCourse}>
-               Load more
-             </Button>
-           )}
-         </div>
-       </div>
-     </>
-   );
- };
+const LoadMoreCourses = () => {
+  const [next, setNext] = useState(coursePerRow);
+
+  const handleMoreCourse = () => {
+    setNext(next + coursePerRow);
+  };
+
+  return (
+    <>
+      <Row className="ms-auto justify-center">
+          {COURSES?.slice(0, next)?.map((course, id) => (
+            <Col md="5" className="m-4" key={id}>
+              <CourseCard course={course} />
+            </Col>
+          ))}
+        {next < COURSES?.length && (
+          <Button className="mt-4" onClick={handleMoreCourse}>
+            Load more
+          </Button>
+        )}
+      </Row>
+    </>
+  );
+};
 
 export default LoadMoreCourses;
