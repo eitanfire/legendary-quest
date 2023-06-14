da0cc14b59c60db09c1eb8b5715aad699fc39fb5
Eitan Fire <eitanfire@gmail.com>
2023-06-13 22:18:57 -0600

    format documents

--- src/features/courses/creditType/Credit.js
+++ src/features/courses/creditType/Credit.js
@@ -1,9 +1,9 @@
 import { Col } from "reactstrap";
-import '../../../Credit.css';
-import COURSES from '../../../app/shared/COURSES';
+import "../../../Credit.css";
+import COURSES from "../../../app/shared/COURSES";
 
-const Credit = ( course ) => {
+const Credit = (course) => {
   return <Col className="credit">{course}</Col>;
-};    
+};
 
 export default Credit;
--- src/features/courses/creditType/Tags.js
+++ src/features/courses/creditType/Tags.js
@@ -1,48 +1,6 @@
-// import { COURSES } from "../../../app/shared/COURSES";
-// import React, { useState } from "react";
-// import Credit from '../../../Credit.css';
-
-// const Tags = () => {
-//   const { credit } = COURSES;
-//   const [input, setInput] = useState("");
-//   const [tags, setTags] = useState([]);
-
-//   const onChange = (e) => {
-//     setInput(e.target.value);
-//   };
-
-//   const onKeyDown = (e) => {
-//     if (e.key === "Enter") {
-//       const trimmedInput = input.trim();
-//       if (trimmedInput && !tags.includes(trimmedInput)) {
-//         setTags((prevState) => [...prevState, trimmedInput]);
-//         setInput("");
-//       }
-//     }
-//   };
-
-//   return (
-//     <div className="container">
-//       {tags.map((tag, index) => (
-//         <div className="tag" key={index}>
-//           {tag}
-//         </div>
-//       ))}
-//       <input
-//         value={input}
-//         placeholder="Enter a tag"
-//         onKeyDown={onKeyDown}
-//         onChange={onChange}
-//       />
-//     </div>
-//   );
-// };
-
-// export default Tags;
-
 import { COURSES } from "../../../app/shared/COURSES";
 import React, { useState } from "react";
-import Credit from '../../../Credit.css';
+import Credit from "../../../Credit.css";
 
 const Tags = () => {
   const { credit } = COURSES;
