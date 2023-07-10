7c621f73a1a32c5ac87b9f518303c684739b9e55
Eitan Fire <eitanfire@gmail.com>
2023-07-09 23:41:37 -0600

    format document

--- src/pages/MovieDayPage.js
+++ src/pages/MovieDayPage.js
@@ -1,12 +1,12 @@
 import { Container } from "reactstrap";
 
 const MovieDayPage = () => {
-    return (
-        <Container>
-    <h1>Movie Day</h1>
-    <h2>Playlists to help bring the content to life</h2>
-        </Container>
-    );
-}
+  return (
+    <Container>
+      <h1>Movie Day</h1>
+      <h2>Playlists to help bring the content to life</h2>
+    </Container>
+  );
+};
 
 export default MovieDayPage;
