e5d67ff16dbb2426ede9cf6a26eab366efad9df5
Eitan Fire <eitanfire@gmail.com>
2023-07-30 23:34:11 -0600

    format documents

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-import { coursesReducer } from '../features/courses/coursesSlice';
-import { takesReducer } from '../features/takes/takesSlice';
-import { rantsReducer } from '../features/rants/rantsSlice';
-import { commentsReducer } from '../features/comments/commentsSlice';
-import { userReducer } from '../features/user/userSlice';
+import { coursesReducer } from "../features/courses/coursesSlice";
+import { takesReducer } from "../features/takes/takesSlice";
+import { rantsReducer } from "../features/rants/rantsSlice";
+import { commentsReducer } from "../features/comments/commentsSlice";
+import { userReducer } from "../features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
--- src/config/get_all_users.js
+++ src/config/get_all_users.js
@@ -1,5 +1,5 @@
 import { collection, getDocs } from "firebase/firestore";
-import { db } from './firestore';
+import { db } from "./firestore";
 
 const querySnapshot = await getDocs(collection(db, "users"));
 querySnapshot.forEach((doc) => {
