192f40d6c5da090b158df8308f1378514ffc31c6
Eitan Fire <eitanfire@gmail.com>
2023-08-05 23:08:35 -0600

    retrieve data from Firestore

--- src/utils/mapImageURL.js
+++ src/utils/mapImageURL.js
@@ -3,8 +3,8 @@ export const mapImageURL = (arr) => {
   return arr.map((item) => {
     return {
       ...item,
-      // image: require("../app/assets/img/" + item.image),
-      image: require("../app/assets/" + item.image),
+      image: require("../app/assets/img/" + item.image),
+      // image: require("../app/assets/" + item.image),
     };
   });
 };
