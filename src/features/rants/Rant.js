1fc3fb85e1bfd1a85e89a1e83a1a0e13525ead73
Eitan Fire <eitanfire@gmail.com>
2023-08-14 23:38:53 -0600

    format

--- src/features/rants/Rant.js
+++ src/features/rants/Rant.js
@@ -1,17 +1,16 @@
 const Rant = ({ rant }) => {
   if (rant) {
     const { image, name, description } = rant;
-         return (
-   <>
-      <img src={image} alt={name} style={{ width: "150px" }}>
-      </img>
-      <div className="m-4">
-        <h5 className="fw-bold">{name}</h5>
-        {description}
-      </div>
-    </>
+    return (
+      <>
+        <img src={image} alt={name} style={{ width: "150px" }}></img>
+        <div className="m-4">
+          <h5 className="fw-bold">{name}</h5>
+          {description}
+        </div>
+      </>
     );
     return null;
-  };
+  }
 };
 export default Rant;
