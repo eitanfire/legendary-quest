3b28c226f0a2e9828bc9086f44538ceab0830e0e
Eitan Fire <eitanfire@gmail.com>
2023-12-25 20:22:25 -0700

    style courses

--- src/features/courses/CourseDetail.js
+++ src/features/courses/CourseDetail.js
@@ -67,9 +67,10 @@ const CourseDetail = ({ course }) => {
           </CardBody>
         </Card>
       </Col>
-      <div className="course-links-container mb-4">
-        <Row>
-          {description && (
+      {description && (
+        <div className="course-links-container mb-4">
+          <Row>
+            {/* <> */}
             <Col className="col col-8 col col-md-9 col-xxl-10">
               <Card className="youtube-playlist">
                 <a href={description} target="_blank" rel="noreferrer">
@@ -77,77 +78,87 @@ const CourseDetail = ({ course }) => {
                 </a>
               </Card>
             </Col>
-          )}
-          <Col>
-            <a
-              href={makeCopyOfDescription(description)}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <button className="btn btn-lg btn-primary">Make a Copy</button>
-            </a>
-          </Col>
-        </Row>
-        <br></br>
-        <Row>
-          {youtube && (
-            <Col>
-              <Card className="youtube-playlist">
-                <a href={youtube} target="_blank" rel="noreferrer">
-                  {icon} YouTube playlist for {name}
-                </a>
-              </Card>
-            </Col>
-          )}
-          {extrayoutube && (
-            <Col>
-              <Card className="youtube-playlist">
-                <a href={extrayoutube} target="_blank" rel="noreferrer">
-                  {icon} Related playlist for {name}
-                </a>
-              </Card>
-            </Col>
-          )}
-          {extrayoutube1 && (
-            <Col>
-              <Card className="youtube-playlist">
-                <a href={extrayoutube1} target="_blank" rel="noreferrer">
-                  {icon} Another related playlist for {name}
-                </a>
-              </Card>
-            </Col>
-          )}
-        </Row>
-        <br></br>
-        {warmups && (
-          <Row>
-            <Col className="col col-8 col col-md-9 col-xxl-10">
-              <Card className="youtube-playlist">
-                <a href={warmups} target="_blank" rel="noreferrer">
-                  {icon} Warm-up questions
-                </a>
-              </Card>
-            </Col>
             <Col>
               <a
-                href={makeCopyOfWarmups(warmups)}
+                href={makeCopyOfDescription(description)}
                 target="_blank"
                 rel="noreferrer"
               >
                 <button className="btn btn-lg btn-primary">Make a Copy</button>
               </a>
             </Col>
-            <div class="g-sharetoclassroom" data-size="32" data-url="..."></div>
+            {/* </> */}
           </Row>
-        )}
-        <Row>
-          {/* <a href="#warm-ups" onClick="renderWidget();">
+          <br></br>
+          <Row>
+            {youtube && (
+              <Col>
+                <Card className="youtube-playlist">
+                  <a href={youtube} target="_blank" rel="noreferrer">
+                    {icon} YouTube playlist for {name}
+                  </a>
+                </Card>
+              </Col>
+            )}
+            {extrayoutube && (
+              <Col>
+                <Card className="youtube-playlist">
+                  <a href={extrayoutube} target="_blank" rel="noreferrer">
+                    {icon} Related playlist for {name}
+                  </a>
+                </Card>
+              </Col>
+            )}
+            {extrayoutube1 && (
+              <Col>
+                <Card className="youtube-playlist">
+                  <a href={extrayoutube1} target="_blank" rel="noreferrer">
+                    {icon} Another related playlist for {name}
+                  </a>
+                </Card>
+              </Col>
+            )}
+          </Row>
+          <br></br>
+          {warmups && (
+            <Row>
+              <Col className="col col-8 col col-md-9 col-xxl-10">
+                <Card className="youtube-playlist">
+                  <a href={warmups} target="_blank" rel="noreferrer">
+                    {icon} Warm-up questions
+                  </a>
+                </Card>
+              </Col>
+              <Col>
+                <a
+                  href={makeCopyOfWarmups(warmups)}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn btn-lg btn-primary">
+                    Make a Copy
+                  </button>
+                </a>
+              </Col>
+              <div
+                class="g-sharetoclassroom"
+                data-size="32"
+                data-url="..."
+              ></div>
+            </Row>
+          )}
+          <Row>
+            {/* <a href="#warm-ups" onClick="renderWidget();">
             Render the Classroom share button
           </a> */}
-               <div id="warm-ups">
-      <div class="g-sharetoclassroom" data-size="32" data-url="..." ></div>
-    </div>
-          {/* <p>
+            <div id="warm-ups">
+              <div
+                class="g-sharetoclassroom"
+                data-size="32"
+                data-url="..."
+              ></div>
+            </div>
+            {/* <p>
             Warm-ups, Bellringers, Skill Drills, Class Openers, Bellwork, Do
             Nows, Entry Tickets, Welcome Work, Questions of the Day.
             <br></br>
@@ -172,14 +183,14 @@ const CourseDetail = ({ course }) => {
             the opportunity to use the warm-up as an entry point into the day's
             lesson.
           </p> */}
-          {/* <g:sharetoclassroom size=32 url="http://google.com"></g:sharetoclassroom> */}
-          {/* <a
+            {/* <g:sharetoclassroom size=32 url="http://google.com"></g:sharetoclassroom> */}
+            {/* <a
             href="https://classroom.google.com/share?url=https://foo.com/"
             topic="Warm-Up Questions"
           >
             Warm-Ups
           </a> */}
-          {/* <Col class="g-sharetoclassroom" data-size="32" data-url="...">
+            {/* <Col class="g-sharetoclassroom" data-size="32" data-url="...">
             <a
               href="https://classroom.google.com/share?url=https://foo.com/"
               topic="Warm-Up Questions"
@@ -188,68 +199,77 @@ const CourseDetail = ({ course }) => {
               Warm-Ups
             </a>
           </Col> */}
-          {/* {warmups && ( */}
-          <div
-            //   className="warmUps"
-            //  explicit
-            class="g-sharetoclassroom"
-            data-size="64"
-            data-theme="classic"
-            data-topic="Warm-Up Questions"
-            data-url="https://docs.google.com/document/d/1dbdDA3tfZp5NugzgwWx2FdOfh7b3U2xHxGbILYKIliA/"
-            //  data-role="presentation"
-            //  data-url={mapassignments}
-            //            data-url1="https://docs.google.com/document/d/1dbdDA3tfZp5NugzgwWx2FdOfh7b3U2xHxGbILYKIliA/edit"
-            //            data-url2="https://docs.google.com/document/d/1cftkj88mYvBn6q3JI5YAJXJgcVvpERpo1nWDzs88wSY/edit"
-            data-itemtype="assignment"
-            data-title="Warm-Ups"
-            data-body="Come to class on time and aim to write at least five (5) sentences or a sketchnote. You may respond to the question, to another prompt of your choice, journal about how it’s going or what you’re grateful for, or add to your own story."
-            // data-copies-only=true
-            // data-role="dialog"
-          >
-            {/* hello */}
-          </div>
-          {/* )} */}
-        </Row>
-        <br></br>
-        {extra && (
-          <Row>
-            <Col className="col col-8 col col-md-9 col-xxl-10">
-              <Card className="youtube-playlist">
-                <a href={extra} target="_blank" rel="noreferrer">
-                  {icon} Resources for Extra Credit and Honors Work
-                </a>
-              </Card>
-            </Col>
-            <Col>
-              <a href={makeCopyOfExtra(extra)} target="_blank" rel="noreferrer">
-                <button className="btn btn-lg btn-primary">Make a Copy</button>
-              </a>
-            </Col>
+            {/* {warmups && ( */}
+            <div
+              //   className="warmUps"
+              //  explicit
+              class="g-sharetoclassroom"
+              data-size="64"
+              data-theme="classic"
+              data-topic="Warm-Up Questions"
+              data-url="https://docs.google.com/document/d/1dbdDA3tfZp5NugzgwWx2FdOfh7b3U2xHxGbILYKIliA/"
+              //  data-role="presentation"
+              //  data-url={mapassignments}
+              //            data-url1="https://docs.google.com/document/d/1dbdDA3tfZp5NugzgwWx2FdOfh7b3U2xHxGbILYKIliA/edit"
+              //            data-url2="https://docs.google.com/document/d/1cftkj88mYvBn6q3JI5YAJXJgcVvpERpo1nWDzs88wSY/edit"
+              data-itemtype="assignment"
+              data-title="Warm-Ups"
+              data-body="Come to class on time and aim to write at least five (5) sentences or a sketchnote. You may respond to the question, to another prompt of your choice, journal about how it’s going or what you’re grateful for, or add to your own story."
+              // data-copies-only=true
+              // data-role="dialog"
+            >
+              {/* hello */}
+            </div>
+            {/* )} */}
           </Row>
-        )}
-        <br></br>
-        {curriculum && (
-          <Row>
-            <Col className="col col-8 col col-md-9 col-xxl-10">
-              <Card className="youtube-playlist">
-                <a href={curriculum} target="_blank" rel="noreferrer">
-                  {icon} Curriculum
+          <br></br>
+          {extra && (
+            <Row>
+              <Col className="col col-8 col col-md-9 col-xxl-10">
+                <Card className="youtube-playlist">
+                  <a href={extra} target="_blank" rel="noreferrer">
+                    {icon} Resources for Extra Credit and Honors Work
+                  </a>
+                </Card>
+              </Col>
+              <Col>
+                <a
+                  href={makeCopyOfExtra(extra)}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn btn-lg btn-primary">
+                    Make a Copy
+                  </button>
                 </a>
-              </Card>
-            </Col>
-            <Col>
-              <a
-                href={makeCopyOfCurriculum(curriculum)}
-                target="_blank"
-                rel="noreferrer"
-              >
-                <button className="btn btn-lg btn-primary">Make a Copy</button>
-              </a>
-            </Col>
-          </Row>
-        )}
-      </div>
+              </Col>
+            </Row>
+          )}
+          <br></br>
+          {curriculum && (
+            <Row>
+              <Col className="col col-8 col col-md-9 col-xxl-10">
+                <Card className="youtube-playlist">
+                  <a href={curriculum} target="_blank" rel="noreferrer">
+                    {icon} Curriculum
+                  </a>
+                </Card>
+              </Col>
+              <Col>
+                <a
+                  href={makeCopyOfCurriculum(curriculum)}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <button className="btn btn-lg btn-primary">
+                    Make a Copy
+                  </button>
+                </a>
+              </Col>
+            </Row>
+          )}
+        </div>
+      )}
     </Container>
   );
 };
