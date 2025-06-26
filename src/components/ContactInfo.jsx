994b79875aae5f1fefabe8869dcc0341f397beea
Eitan Fire <eitanfire@gmail.com>
2025-06-25 22:39:37 -0600

    delete comments

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -21,30 +21,6 @@ const ContactInfo = () => {
 
   return (
     <div>
-      {/* <a
-        href="https://drive.google.com/file/d/1X7FGKxASFayUJf7Xf5le-ZqC1JzrfKXO/view?usp=sharing"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i className="fa-solid fa-image-portrait fa-sm"></i> Resume
-      </a>
-      {" • "}
-      <a href="https://github.com/eitanfire" target="_blank" rel="noreferrer">
-        <i className="fa fa-github"></i> GitHub
-      </a>
-      {" • "}
-      <a
-        href="https://www.linkedin.com/in/eitanfire/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <i className="fa fa-linkedin"></i> LinkedIn
-      </a>
-      {" • "}
-      <a href="tel:+13038177843">
-        <i className="fa fa-phone"></i> Phone
-      </a>
-      {" • "} */}
       <a href="mailto:teachleagueabc@gmail.com">
         <i className="fa fa-envelope-o"></i> Email
       </a>{" "}
