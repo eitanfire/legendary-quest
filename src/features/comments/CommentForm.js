8a9c41aa4c71ce2fd1d564c8bce16729f7ae6b1e
Eitan Fire <eitanfire@gmail.com>
2025-09-06 22:47:35 -0600

    delete import

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -12,7 +12,6 @@ import {
 import { Formik, Field, Form, ErrorMessage } from "formik";
 import { validateCommentForm } from "../../utils/validateCommentForm";
 import { useDispatch } from "react-redux";
-// import { addComment, postComment } from "./commentsSlice";
 
 const CommentForm = ({ courseId }) => {
   const [modalOpen, setModalOpen] = useState(false);
